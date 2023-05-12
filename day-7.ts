06b3ade82ba8906aaf79cef466f0638d6e0a1307
Anshuman Singh <anshumansingh005@gmail.com>
2023-05-12 16:12:01 +0530

    TS solution added

--- day-7.ts
+++ day-7.ts
@@ -9,3 +9,18 @@ function compose1(functions: F[]): F {
     return x;
   };
 }
+
+// Approach 1 = using reduceRight;
+/**
+ Here's a breakdown of how Array.reduceRight() works in the context of the compose function:
+
+The compose function receives an array of functions and returns a new function that takes an input value x.
+When the new function is called with an input value x, it calls Array.reduceRight() on the functions array.
+The reducer function is called for each function in the array, starting from the rightmost element and moving towards the left. The accumulator initially holds the input value x.
+In each iteration, the reducer function applies the current function to the accumulator and updates the accumulator with the result.
+Once all the functions have been applied, the final value of the accumulator is returned.
+ */
+type Fun = (x: number) => number;
+function compose2(functions: Fun[]): Fun {
+  return (x: number): number => functions.reduceRight((acc, f) => f(acc), x);
+}
